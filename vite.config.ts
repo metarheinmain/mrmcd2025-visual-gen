75a17eee60217940d7ae301de3af42eb7930c107
Leif Niemczik <leif.niem@gmail.com>
2025-08-27 23:43:36 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
-import { ssamExport } from "vite-plugin-ssam-export";
-import { ssamFfmpeg } from "vite-plugin-ssam-ffmpeg";
 import { ssamGit } from "vite-plugin-ssam-git";
-// import { ssamTimelapse } from "vite-plugin-ssam-timelapse";
 
 export default defineConfig({
   base: "./",
@@ -11,17 +8,17 @@ export default defineConfig({
     glsl({
       warnDuplicatedImports: false,
     }),
-    ssamExport(),
-    ssamFfmpeg(),
     ssamGit(),
-    // ssamTimelapse(),
   ],
   clearScreen: false,
   build: {
     outDir: "./dist",
     assetsDir: ".",
     rollupOptions: {
-      //
+      input: {
+        main: "index.html",
+        konfigurator: "konfigurator/index.html"
+      }
     },
   },
   server: {
