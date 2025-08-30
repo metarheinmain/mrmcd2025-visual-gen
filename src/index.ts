56315f14efa46201c7363766aa4128c599d43e32
Leif <leif.niem@gmail.com>
2025-08-30 21:09:46 +0200

    typing fixes

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,15 @@ import {
   OrthographicCamera,
   Scene,
   ShaderMaterial,
-  SRGBColorSpace,
   TextureLoader,
   Vector2,
   WebGLRenderer,
 } from "three";
-import {saveAs} from "file-saver"
 import baseVert from "./shaders/base.vert";
 import baseFrag from "./shaders/base.frag";
 import { snapdom } from "@zumer/snapdom";
-import Croppie from "croppie"
-import 'croppie/croppie.css'
+import Croppie from "croppie";
+import "croppie/croppie.css";
 
 const sketch: Sketch<"webgl2"> = async ({
   wrap,
@@ -30,34 +28,46 @@ const sketch: Sketch<"webgl2"> = async ({
     import.meta.hot.accept(() => wrap.hotReload());
   }
 
-  const renderer = new WebGLRenderer({ canvas, "antialias": true, "alpha": true });
+  const renderer = new WebGLRenderer({ canvas, antialias: true, alpha: true });
   renderer.setSize(width, height);
   renderer.setPixelRatio(pixelRatio);
 
   const camera = new OrthographicCamera(-1, 1, 1, -1, 0, 1);
   const scene = new Scene();
 
-  const uvMap = new TextureLoader().load("/uv_book.png") 
-  const lightingTexture = new TextureLoader().load("/lighting_book.png") 
-  const userImage = new TextureLoader().load("/placeholder.jpg") 
+  const uvMap = new TextureLoader().load("/uv_book.png");
+  const lightingTexture = new TextureLoader().load("/lighting_book.png");
+  const userImage = new TextureLoader().load("/placeholder.jpg");
 
   const uploadInput = document.getElementById("upload") as HTMLInputElement;
   const cropModal = document.getElementById("crop-modal") as HTMLDivElement;
   const cropImage = document.getElementById("crop-image") as HTMLImageElement;
-  const cropButton = document.getElementById("crop-button") as HTMLButtonElement;
+  const cropButton = document.getElementById(
+    "crop-button",
+  ) as HTMLButtonElement;
   let cropper: Croppie;
   const filenameInput = document.getElementById("filename") as HTMLInputElement;
-  const filenameDisplay = document.getElementsByClassName("filename")[0] as HTMLDivElement;
-  const backgroundSelect = document.getElementById("background") as HTMLInputElement;
+  const filenameDisplay = document.getElementsByClassName(
+    "filename",
+  )[0] as HTMLDivElement;
+  const backgroundSelect = document.getElementById(
+    "background",
+  ) as HTMLInputElement;
   const sloganToggle = document.getElementById("slogan") as HTMLInputElement;
-  const slogan = document.querySelector(".motto")
+  const slogan = document.querySelector(".motto");
   const svg = document.getElementById("image") as HTMLInputElement;
   const exportButton = document.getElementById("saveImage") as HTMLInputElement;
 
-  const aspectRatioSelect = document.getElementById("aspect-ratio") as HTMLInputElement;
+  const aspectRatioSelect = document.getElementById(
+    "aspect-ratio",
+  ) as HTMLInputElement;
   const motto = (svg as any).querySelector("svg");
-  const canvasContainerFO = (svg as any).querySelector("foreignObject:nth-of-type(1)") as SVGForeignObjectElement;
-  const filenameContainerFO = (svg as any).querySelector("foreignObject:nth-of-type(2)") as SVGForeignObjectElement;
+  const canvasContainerFO = (svg as any).querySelector(
+    "foreignObject:nth-of-type(1)",
+  ) as SVGForeignObjectElement;
+  const filenameContainerFO = (svg as any).querySelector(
+    "foreignObject:nth-of-type(2)",
+  ) as SVGForeignObjectElement;
   const eventName = (svg as any).querySelector(".event-name") as SVGTextElement;
 
   aspectRatioSelect.addEventListener("change", (event) => {
@@ -67,7 +77,7 @@ const sketch: Sketch<"webgl2"> = async ({
       case "square":
         sloganToggle.disabled = true;
         svg.setAttribute("viewBox", "0 0 150 150");
-        motto.style.display = 'none'
+        motto.style.display = "none";
         canvasContainerFO.setAttribute("x", "10");
         canvasContainerFO.setAttribute("y", "0");
         canvasContainerFO.setAttribute("width", "130");
@@ -81,7 +91,7 @@ const sketch: Sketch<"webgl2"> = async ({
       case "threeByFour":
         sloganToggle.disabled = false;
         svg.setAttribute("viewBox", "0 0 150 200");
-        motto.style.display = sloganToggle.checked ? 'block' : 'none'
+        motto.style.display = sloganToggle.checked ? "block" : "none";
         motto.setAttribute("x", "23");
         motto.setAttribute("y", "20");
         canvasContainerFO.setAttribute("x", "0");
@@ -97,7 +107,7 @@ const sketch: Sketch<"webgl2"> = async ({
       case "din":
         sloganToggle.disabled = false;
         svg.setAttribute("viewBox", "0 0 150 212");
-        motto.style.display = sloganToggle.checked ? 'block' : 'none'
+        motto.style.display = sloganToggle.checked ? "block" : "none";
         motto.setAttribute("x", "23");
         motto.setAttribute("y", "20");
         canvasContainerFO.setAttribute("x", "0");
@@ -113,7 +123,7 @@ const sketch: Sketch<"webgl2"> = async ({
       case "widescreen":
         sloganToggle.disabled = false;
         svg.setAttribute("viewBox", "0 0 250 150");
-        motto.style.display = sloganToggle.checked ? 'block' : 'none'
+        motto.style.display = sloganToggle.checked ? "block" : "none";
         motto.setAttribute("x", "10");
         motto.setAttribute("y", "10");
         canvasContainerFO.setAttribute("x", "125");
@@ -129,9 +139,9 @@ const sketch: Sketch<"webgl2"> = async ({
     }
   });
 
-  filenameDisplay.innerHTML = filenameInput.value
+  filenameDisplay.innerHTML = filenameInput.value;
 
-  uploadInput.addEventListener("change", (event) => {
+  uploadInput.addEventListener("change", () => {
     const file = uploadInput.files?.[0];
     if (file) {
       const reader = new FileReader();
@@ -143,7 +153,7 @@ const sketch: Sketch<"webgl2"> = async ({
             viewport: {
               width: 500,
               height: 500,
-              type: "square"
+              type: "square",
             },
             enforceBoundary: true,
           });
@@ -154,35 +164,37 @@ const sketch: Sketch<"webgl2"> = async ({
   });
 
   filenameInput.addEventListener("keyup", (event) => {
-    if (event.key === " ") filenameInput.value += " "
-    filenameDisplay.innerHTML = filenameInput.value
-  })
+    if (event.key === " ") filenameInput.value += " ";
+    filenameDisplay.innerHTML = filenameInput.value;
+  });
 
-
-  sloganToggle.addEventListener("change", (event) => {
-    if(sloganToggle.checked) {
-      motto.style.display = "block"
+  sloganToggle.addEventListener("change", () => {
+    if (sloganToggle.checked) {
+      motto.style.display = "block";
     } else {
-      motto.style.display = "none"
+      motto.style.display = "none";
     }
-  })
+  });
 
   backgroundSelect.addEventListener("change", (event) => {
     const color = (event.target as HTMLInputElement).value;
-    
-    if(color === "white") {
-      svg.style.setProperty("--image-bg", "#fff")
-      svg.style.setProperty("--text-color", "#000")
-    } else if(color === "black") {
-      svg.style.setProperty("--image-bg", "#000")
-      svg.style.setProperty("--text-color", "#fff")
+
+    if (color === "white") {
+      svg.style.setProperty("--image-bg", "#fff");
+      svg.style.setProperty("--text-color", "#000");
+    } else if (color === "black") {
+      svg.style.setProperty("--image-bg", "#000");
+      svg.style.setProperty("--text-color", "#fff");
     }
-  })
+  });
 
   cropButton.addEventListener("click", async () => {
-    const croppedCanvas = await cropper.result({ type: "rawcanvas", size: "original" });
+    const croppedCanvas = await cropper.result({
+      type: "rawcanvas",
+      size: "original",
+    });
     const image = croppedCanvas.toDataURL();
-    const newTexture = new TextureLoader().load(image, (texture) => {
+    new TextureLoader().load(image, (texture) => {
       texture.needsUpdate = true;
       uniforms.userImage.value = texture;
       if (userImage) {
@@ -190,32 +202,45 @@ const sketch: Sketch<"webgl2"> = async ({
       }
     });
     cropModal.style.display = "none";
-    svg.style.setProperty("--image", `url(${image})`)
+    svg.style.setProperty("--image", `url(${image})`);
     cropper.destroy();
   });
 
-    const dimensions: Record<string, {width: number, height: number}> = {
-      "square": {width: 2500, height: 2500},
-      "threeByFour": {width: 2500, height: 3333 },
-      "din": {width: 2500, height: 3536},
-      "widescreen": {width: 1920, height: 1080},
-    }
+  const dimensions: Record<string, { width: number; height: number }> = {
+    square: { width: 2500, height: 2500 },
+    threeByFour: { width: 2500, height: 3333 },
+    din: { width: 2500, height: 3536 },
+    widescreen: { width: 1920, height: 1080 },
+  };
 
   exportButton.addEventListener("click", async () => {
     exportButton.disabled = true;
-    const canvasContainer = document.querySelector(".canvasContainer") as HTMLCanvasElement
-    const canvas = document.querySelector(".canvasContainer canvas") as HTMLCanvasElement
-    const tempImage = canvas.toDataURL("image/png")
-    canvasContainer.style.background = `url('${tempImage}') center`
-    canvasContainer.style.backgroundSize = "contain"
-    const finalDimensions = dimensions[aspectRatioSelect.querySelector("[checked]").value]
-   console.log(svg);
-    
-    const snapdomCanvas = await snapdom.download(svg, { scale: finalDimensions.width / svg.getBoundingClientRect().width, embedFonts: true, reset: "hard", filename: `mrmcd_poster_${(new Date()).toLocaleString()}.png`});
-    canvasContainer.style.background = `none`
+    const canvasContainer = document.querySelector(
+      ".canvasContainer",
+    ) as HTMLDivElement;
+    const canvas = document.querySelector(
+      ".canvasContainer canvas",
+    ) as HTMLCanvasElement;
+    const tempImage = canvas.toDataURL("image/png");
+    canvasContainer.style.background = `url('${tempImage}') center`;
+    canvasContainer.style.backgroundSize = "contain";
+    const finalDimensions =
+      dimensions[
+        (aspectRatioSelect.querySelector("[checked]") as HTMLInputElement)
+          .value ?? "din"
+      ];
+    console.log(svg);
+
+    const snapdomCanvas = await snapdom.download(svg, {
+      scale: finalDimensions.width / svg.getBoundingClientRect().width,
+      embedFonts: true,
+      reset: "hard",
+      filename: `mrmcd_poster_${new Date().toLocaleString()}.png`,
+    });
+    canvasContainer.style.background = `none`;
     // canvasContainer.style.display = "block"
     exportButton.disabled = false;
-  })
+  });
 
   const textureSets = [
     "book",
@@ -230,10 +255,12 @@ const sketch: Sketch<"webgl2"> = async ({
     "website",
   ];
 
-  const textureSelect = document.getElementById("texture-select") as HTMLSelectElement;
+  const textureSelect = document.getElementById(
+    "texture-select",
+  ) as HTMLSelectElement;
 
-  textureSelect.innerHTML = ""
-  textureSets.forEach(setName => {
+  textureSelect.innerHTML = "";
+  textureSets.forEach((setName) => {
     const option = document.createElement("option");
     option.value = setName;
     option.text = setName;
@@ -256,15 +283,15 @@ const sketch: Sketch<"webgl2"> = async ({
         lightingTexture.dispose();
       }
     });
-  }); 
+  });
 
   const geometry = new PlaneGeometry(2, 2);
   const uniforms = {
     resolution: { value: new Vector2(width, height) },
     time: { value: 0.0 },
-    uvMap: { value:  uvMap},
-    lightingTexture: { value:  lightingTexture},
-    userImage: { value:  userImage},
+    uvMap: { value: uvMap },
+    lightingTexture: { value: lightingTexture },
+    userImage: { value: userImage },
   };
   const material = new ShaderMaterial({
     vertexShader: baseVert,
@@ -288,7 +315,6 @@ const sketch: Sketch<"webgl2"> = async ({
     renderer.forceContextLoss();
   };
 };
-
 
 const settings: SketchSettings = {
   mode: "webgl2",
